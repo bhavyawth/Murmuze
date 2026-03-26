bdda4ac701c1f7dc7453753354905660c477e672
Bhavya <bbhojwani31@gmail.com>
2026-03-27 00:15:51 +0530

    prepare for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,20 +9,19 @@ import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 
 dotenv.config();
-const PORT = process.env.PORT;
 
 app.use(cookieParser());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.CLIENT_URL,
   credentials: true,
 }))
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
+server.listen(process.env.PORT || 3000, '0.0.0.0', () => {
+  console.log(`Server is running on port: ${process.env.PORT || 3000}`);
   connectDB();
 })
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
+const API_BASE_URL = `${import.meta.env.VITE_API_URL || import.meta.env.REACT_APP_API_URL}/api`;
+
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001/api', 
+  baseURL: API_BASE_URL,
   withCredentials: true,
 });
