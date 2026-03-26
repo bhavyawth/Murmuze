6cc69e97e9a41a8a3d3d327438876ee88b40d253
Bhavya <bbhojwani31@gmail.com>
2026-03-27 00:34:23 +0530

    add Dockerfile for backend deployment & remove log statements

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -22,15 +22,12 @@ function getRecieverSocketId(userId) {
 }
 
 io.on('connection', (socket) => {
-  console.log("A user connected", socket.id);
-
   const userId = socket.handshake.query.userId;
   if (userId) userSocketMap[userId] = socket.id;
 
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
-    console.log("A user disconnected", socket.id);
     delete userSocketMap[userId];
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
